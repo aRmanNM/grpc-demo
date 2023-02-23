1ea044eb305722ee195706bb56e8de5a293d0179
armannm <anm4500@gmail.com>
2023-02-24 00:07:46 +0330

    add streaming samples

--- src/client/Program.cs
+++ src/client/Program.cs
@@ -1,4 +1,5 @@
 ﻿using client;
+using Grpc.Core;
 using Grpc.Net.Client;
 using Spectre.Console;
 
@@ -22,6 +23,9 @@ internal class Program
         {
             Console.WriteLine("Specify server action:");
             Console.WriteLine("0 - say hello");
+            Console.WriteLine("1 - server streaming counter");
+            Console.WriteLine("2 - client streaming summation");
+            Console.WriteLine("3 - bidirectional ping/ping");
             Console.WriteLine("exit - Exit the program");
 
             var action = Console.ReadLine();
@@ -33,6 +37,15 @@ internal class Program
             {
                 case "0":
                     await SayHello(client);
+                    break;
+                case "1":
+                    await StreamServerCounter(client);
+                    break;
+                case "2":
+                    await StreamClientSummation(client);
+                    break;
+                case "3":
+                    await StreamBidirectionalPingPong(client);
                     break;
                 default:
                     Console.WriteLine("Invalid action specified");
@@ -46,11 +59,88 @@ internal class Program
         }
     }
 
+    private static async Task StreamBidirectionalPingPong(Demo.DemoClient client)
+    {
+        Console.Write("iterations: ");
+        int.TryParse(Console.ReadLine(), out int iterations);
+
+        Console.Write("\n ----- \nResult:\t");
+
+        using var call = client.StreamBothWays();
+
+        var readTask = Task.Run(async () =>
+        {
+            await foreach (var response in call.ResponseStream.ReadAllAsync())
+            {
+                Console.Write("{0}\t", response.Message);
+            }
+        });
+
+        int counter = 0;
+        string msg = "Ping";
+
+        while (counter < iterations)
+        {
+            Console.Write("{0}\t", msg);
+            await call.RequestStream.WriteAsync(new PingRequest { Message = msg });
+            await Task.Delay(TimeSpan.FromSeconds(1));
+            counter++;
+        }
+
+        await call.RequestStream.CompleteAsync();
+        await readTask;
+
+        Console.WriteLine("\n ----- \n");
+    }
+
+    private static async Task StreamClientSummation(Demo.DemoClient client)
+    {
+        Console.Write("numbers (seperated by space): ");
+        var numbers = Console.ReadLine()?
+            .Split(",")
+            .Select(x => int.Parse(x)).ToList() ?? new List<int>() { 1, 2, 3 };
+
+        using var call = client.StreamFromClient();
+
+        foreach (var number in numbers)
+            await call.RequestStream.WriteAsync(new NumbersRequest { Number = number });
+
+        await call.RequestStream.CompleteAsync();
+
+        var response = await call;
+        Console.WriteLine("\n ----- \nServer response is: sum equals {0} and numbers are {1}\n ----- \n", response.Sum, response.Numbers);
+    }
+
+    private static async Task StreamServerCounter(Demo.DemoClient client)
+    {
+        int start = 1;
+        int finish = 5;
+
+        Console.Write("start: ");
+        int.TryParse(Console.ReadLine(), out start);
+
+        Console.Write("finish: ");
+        int.TryParse(Console.ReadLine(), out finish);
+
+        using var call = client.StreamFromServer(new CounterRequest
+        {
+            Start = start,
+            Finish = finish
+        });
+
+        Console.Write("\n ----- \nServer response is:\t");
+
+        await foreach (var response in call.ResponseStream.ReadAllAsync())
+            Console.Write("{0}\t", response.Counter);
+
+        Console.WriteLine("\n ----- \n");
+    }
+
     private static async Task SayHello(Demo.DemoClient client)
     {
-        var reply = await client.SayHelloAsync(
+        var response = await client.SayHelloAsync(
                     new HelloRequest { Name = "console_client" });
 
-        Console.WriteLine("\n ----- \nServer response is: {0}\n ----- \n", reply.Message);
+        Console.WriteLine("\n ----- \nServer response is: {0}\n ----- \n", response.Message);
     }
 }
--- src/server/Services/DemoService.cs
+++ src/server/Services/DemoService.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Google.Protobuf.Collections;
 using Grpc.Core;
 
 namespace server.Services;
@@ -14,5 +15,51 @@ public class DemoService : Demo.DemoBase
         {
             Message = $"hello {request.Name}"
         });
+    }
+
+    public override async Task StreamFromServer(CounterRequest request, IServerStreamWriter<CounterResponse> responseStream, ServerCallContext context)
+    {
+        var counter = request.Start;
+        while (!context.CancellationToken.IsCancellationRequested && counter <= request.Finish)
+        {
+            await responseStream.WriteAsync(new CounterResponse
+            {
+                Counter = counter
+            });
+
+            if (counter == request.Finish)
+                break;
+
+            counter++;
+            await Task.Delay(TimeSpan.FromSeconds(1), context.CancellationToken);
+        }
+    }
+
+    public override async Task<SumResponse> StreamFromClient(IAsyncStreamReader<NumbersRequest> requestStream, ServerCallContext context)
+    {
+        var sum = 0;
+        var numbers = new RepeatedField<int>();
+        await foreach (var request in requestStream.ReadAllAsync())
+        {
+            sum += request.Number;
+            numbers.Add(request.Number);
+        }
+
+        var response = new SumResponse();
+        response.Numbers.Add(numbers);
+        response.Sum = sum;
+
+        return response;
+    }
+
+    public override async Task StreamBothWays(IAsyncStreamReader<PingRequest> requestStream, IServerStreamWriter<PongResponse> responseStream, ServerCallContext context)
+    {
+        await foreach (var request in requestStream.ReadAllAsync())
+        {
+            await responseStream.WriteAsync(new PongResponse
+            {
+                Message = "Pong"
+            });
+        }
     }
 }
